847a4a6261f7d0ab3cc30ed4c687007376908701
mamaj@macbook <mamareza.e@gmail.com>
2022-09-20 23:16:32 -0400

    read file from new exp

--- 1_create_delay_profiles/main2.py
+++ 1_create_delay_profiles/main2.py
@@ -1,26 +1,36 @@
-import pandas as pd
 from pathlib import Path
 
+import pandas as pd
+import numpy as np
+from tqdm.notebook import tqdm, trange
+
+from utils import slugify
 from run import run 
 
 
+folder = 'sam-gc-cnn_profile_est_desktop_long2'
+sam_name = 'sam-gc-cnn'
 
-mu = 0.2
+# invokes = 40
+invokes = 40
 
-workers = n = 256
-rounds = 10  # number of jobs to complete
-ninvokes = 300
-batch = 256
-folder = 'vgg16_profile'
+workers = 256
+batch = 4096
 comp_type = 'no_forloop'
+region = 'Canada'
 
-df = pd.read_csv(folder + '/best_params.csv')
+# n_jobs = 30  # number of jobs to complete
+# base_load = 0.0
+# mu = 1.0
+
+fname = '../delay_profiles/sam-gc-cnn_profile_est_desktop_long2/mu1_000-base_load0_000-njobs30-base_comp2_115-Canada.csv'
+df = pd.read_csv(fname, index_col=0)
+
 
 if __name__ == '__main__':
 
-    for i, row in df.iterrows():
-        print(row)
-        region = row['region']
-        load = row['load']
-        run(workers, ninvokes, load, batch, comp_type, region, folder, dryrun=1, suffix=i)
-    
+    loads = df['load']
+    for load in df['load']:
+        print(load)
+        run(workers, invokes, load, batch, comp_type, region, sam_name, folder, dryrun=2)
+        
