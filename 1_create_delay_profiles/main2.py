c05b49f7a784818376cd844784cc95dc482eb62a
mamaj@RezaDesktop <mamareza.e@gmail.com>
2022-09-23 08:41:35 -0400

    long4 real

--- 1_create_delay_profiles/main2.py
+++ 1_create_delay_profiles/main2.py
@@ -8,11 +8,11 @@ from utils import slugify
 from run import run 
 
 
-folder = 'profile_est_desktop_long2'
+folder = 'profile_est_desktop_long4_real'
 sam_name = 'sam-gc-cnn'
 
 # invokes = 40
-invokes = 40
+invokes = 100
 
 workers = 256
 batch = 4096
@@ -23,7 +23,8 @@ region = 'Canada'
 # base_load = 0.0
 # mu = 1.0
 
-fname = '../delay_profiles/sam-gc-cnn_profile_est_desktop_long2/mu1_000-base_load0_000-njobs30-base_comp2_115-Canada.csv'
+# fname = '../delay_profiles/sam-gc-cnn_profile_est_desktop_long2/mu1_000-base_load0_000-njobs30-base_comp2_115-Canada.csv'
+fname = '../delay_profiles/sam-gc-cnn_profile_est_desktop_long4/mu1_000-base_load0_000-njobs80-base_comp2_166-Canada.csv'
 df = pd.read_csv(fname, index_col=0)
 
 
@@ -32,5 +33,5 @@ if __name__ == '__main__':
     loads = df['load']
     for load in tqdm(df['load']):
         print(load)
-        run(workers, invokes, load, batch, comp_type, region, sam_name, folder, dryrun=2, suffix=3)
+        run(workers, invokes, load, batch, comp_type, region, sam_name, folder, dryrun=2, suffix=1)
         
